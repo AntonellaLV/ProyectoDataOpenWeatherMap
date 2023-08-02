37c4c3700584f808ef1bd806c73fed64df2f226f
AntonellaLV <118840064+AntonellaLV@users.noreply.github.com>
2023-08-02 16:52:12 -0300

    Actualizar config.py

--- ProyectoAnalisisDeDatos23/medio y avanzado/config.py
+++ ProyectoAnalisisDeDatos23/medio y avanzado/config.py
@@ -1,5 +1,5 @@
 # Clave de la API de OpenWeatherMap
-API_KEY = "0b78802c8ee6bd94e77033d1dc131399"
+API_KEY = # "0b78802c8ee6bd94e77033d1dc131399"
 # Configuración de conexión a la base de datos
 DB_USER = "postgres"
 DB_PASSWORD = "admin"
